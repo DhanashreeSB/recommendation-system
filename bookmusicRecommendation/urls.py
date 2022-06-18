fd93cad9f7af44e960487f158e7e8683bd9d9968
DhanashreeSB <123dhanashri@gmail.com>
2022-06-18 21:28:05 +0530

    Deployment changes

--- bookmusicRecommendation/urls.py
+++ bookmusicRecommendation/urls.py
@@ -27,6 +27,8 @@ urlpatterns = [
     path('about/',about_view,name='about'),
     path('contact/',contact_view,name='contact'),
     path('admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
